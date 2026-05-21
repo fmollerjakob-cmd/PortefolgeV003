a98eace3a4f1711527aacd78a9da3bb1808eee65
Jakob <jakob@friborgmoeller.dk>
2026-05-21 21:43:57 +0200

    Adding basic shit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::cout << "Hello Shitty World, Logging into git on school account makes me wanna kill myself, a joke abviously!" << std::endl;
+
+}
